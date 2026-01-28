c9b827994f2eb6067cc0a741017ece2ce3b980d1
sahnunscripted <sahnunhm@gmail.com>
2026-01-28 12:42:00 -0500

    Fix select width and remove duplicate privacy statement from auth page

--- client/src/components/common/Select/Select.jsx
+++ client/src/components/common/Select/Select.jsx
@@ -34,7 +34,7 @@ const Select = forwardRef(({
           {label}
         </label>
       )}
-      <div className="relative">
+      <div className="relative"  style={{width: '105px'}}>
         <select
           ref={ref}
           className={`${selectClasses} ${className}`}
